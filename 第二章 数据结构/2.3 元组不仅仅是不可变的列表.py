63f34b6643a7b7dd78145a1abdb01ac43de5f95b
KIIIIK <2371066459@qq.com>
2020-12-16 21:55:02 +0800

    2020-12-16 21:54

--- 第二章 数据结构/2.3 元组不仅仅是不可变的列表.py
+++ 第二章 数据结构/2.3 元组不仅仅是不可变的列表.py
@@ -18,6 +18,36 @@ a, b, rest
 a, *body, c, d = range(5)
 a, body, c, d
 
+#用嵌套元组来获取经度
+metro_areas = [
+    ('Tokyo', 'JP', 36.933, (35.689722, 139.691667)),
+    ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
+    ('Mexico City', 'MX', 20.142, (19.433333, -99.133333)),
+    ('New York-Newark', 'US', 20.104, (40.808611, -74.020386)),
+    ('Sao Paulo', 'BR', 19.649, (-23.547778, -46.635833))
+]
+print('{:15} | {:^9}'.format('', 'lat.', 'long.'))
+fmt = '{:15} | {:9.4f} | {:9.4f}'
+for name, cc, pop, (latitude, longitude) in metro_areas:
+    if longitude <= 0:
+        print(fmt.format(name, latitude, longitude))
 
-
-
+"""
+2.3.4 具名元组
+"""
+#定义和使用具名元组
+from collections import namedtuple
+City = namedtuple('City', 'name country population coordinates')
+tokyo = City('Tokyo', 'JP',36.933, (35.689722, 139.691667))
+tokyo
+tokyo.population
+tokyo.coordinates
+tokyo[1]
+#具名元组的属性和方法
+City._fields
+LatLong = namedtuple('LatLong', 'lat long')
+delhi_data = ('Delhi NCR', 'IN', 21.935, LatLong(28.613889, 77.208889))
+delhi = City._make(delhi_data)
+delhi._asdict()
+for key, value in delhi._asdict().items():
+    print(key + ':', value)
