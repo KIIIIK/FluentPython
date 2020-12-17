c7077db6914ae7f5f38abc11d25b1a0d263a0f4e
KIIIIK <2371066459@qq.com>
2020-12-17 22:02:27 +0800

    2020-12-17 22:02

---
+++ 第二章 数据结构/2.7 list.sort方法和内置函数sorted.py
@@ -0,0 +1,23 @@
+fruits = ['grape', 'respberry', 'apple', 'banana']
+sorted(fruits)
+fruits
+sorted(fruits, reverse=True)
+sorted(fruits, key=len)
+sorted(fruits, key=len, reverse=True)
+fruits
+fruits.sort()
+fruits
+
+
+
+
+
+
+
+
+
+
+
+
+
+
