b3bd32ce277be8f36d404edf53b3f71a31c58864
KIIIIK <2371066459@qq.com>
2020-12-20 20:34:30 +0800

    2020-12-20 20:34

--- 第二章 数据结构/2.9 当列表不是首选时.py
+++ 第二章 数据结构/2.9 当列表不是首选时.py
@@ -15,16 +15,44 @@ fp.close()
 floats2[-1]
 floats2 == floats
 
+"""
+2.9.2 内存试图
+"""
+numbers = array('h', [-2, -1, 0, 1, 2])
+memv = memoryview(numbers)
+len(memv)
+memv[0]
+memv_oct = memv.cast('B')
+memv_oct.tolist()
+memv_oct[5] = 4
+numbers
 
+"""
+2.9.3 NumPy和SciPy
+"""
+import numpy
+floats = numpy.loadtxt('floats-10M-lines.txt')
+#精度和性能都较高的计时器
+from time import perf_counter as pc
 
-
-
-
-
-
-
-
-
-
+"""
+2.9.4 双向队列和其他形式的队列
+"""
+from collections import deque
+dq = deque(range(10), maxlen=10)
+dq
+dq.rotate(3)
+dq
+dq.rotate(-4)
+dq
+dq.appendleft(-1)
+dq
+dq.extend([11, 22, 33])
+dq
+dq.extendleft([10, 20, 30, 40])
+dq
+#其他队列库
+##queue,multiprocessing,asyncio,heapq
+#排序算法Timsort
 
 
