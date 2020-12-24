2ceb7bc1ad7ece8c5e27b61229e434c54380135b
KIIIIK <2371066459@qq.com>
2020-12-24 21:44:41 +0800

    2020-12-24 21:44

--- 第三章 字典和集合/3.8 集合论.py
+++ 第三章 字典和集合/3.8 集合论.py
@@ -1,9 +1,29 @@
+l = ['spam', 'spam', 'eggs', 'spam']
+set(l)
+list(set(l))
 
+#a | b返回合集,a & b返回交集,a - b返回差集
 
+found = len(set(needles) & set(haystack))
+found = len(set(needles).intersection(hystack))
 
+"""
+3.8.1 集合字面量
+"""
+s = {1}
+type(s)
+s
+s.pop()
+s
+from dis import dis
+dis('{1}')
+dis('set([1])')
 
-
-
+"""
+3.8.2 集合推导
+"""
+from unicodedata import name
+{chr(i) for i in range(32, 256) if 'SIGN' in name(chr(i), '')}
 
 
 
