9ad75868f890c48f57858b50bb4cc014bd99dfb3
KIIIIK <2371066459@qq.com>
2021-01-12 22:42:41 +0800

    2021-01-12 22:42

--- 第五章 一等函数/5.10 支持函数式编程的包.py
+++ 第五章 一等函数/5.10 支持函数式编程的包.py
@@ -12,14 +12,20 @@ def fact(n):
     return reduce(mul, range(1, n+1))
 
 
+from operator import methodcaller
+s = 'The time has come'
+upcase = methodcaller('upper')
+upcase(s)
+hiphenate = methodcaller('replace', ' ', '-')
+hiphenate(s)
+str.upper(s)
 
-
-
-
-
-
-
-
+#使用functools.partial冻结参数
+from operator import mul
+from functools import partial
+triple = partial(mul, 3)
+triple(7)
+list(map(triple, range(1, 10)))
 
 
 
